5ef5a9acaecd868cd33c6c3eb8d7f582397fa9a1
aisyah-t <aisyah.n.tajudin@gmail.com>
2020-09-20 23:10:42 +1200

    Change app content, add ReadLine

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace c_sharp_console_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "C# Console App";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 30;
+
+            Console.WriteLine("Hello, what is your name?\nMy name is Mr Miyagi.");
+            Console.ReadLine();
+
+            Console.WriteLine("Nice to meet you!\nWhat's your favourite colour?");
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Cool! Mine is red.");
+
             Console.ReadKey();
         }
     }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # c-sharp-console-app
 
-## My first exploration into C# & .NET
+### My first exploration into C# & .NET
 
 <br>:seedling: Exploring _**C#**_ and _**.NET**_ using [Brackeys](https://www.youtube.com/playlist?list=PLPV2KyIb3jR4CtEelGPsmPzlvP7ISPYzR) tutorials.
